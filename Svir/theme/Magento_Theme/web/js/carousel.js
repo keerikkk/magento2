24c5f2d2329401ea1d06fe4bf1b8f052b352f59c
Kyrylo Svir <ksvir17@gmail.com>
2025-03-05 15:54:33 +0200

    STD-665 - Carousel responsiveness
    - Add responsiveness for carousel
    - Change image display styles

--- Svir/theme/Magento_Theme/web/js/carousel.js
+++ Svir/theme/Magento_Theme/web/js/carousel.js
@@ -2,8 +2,22 @@ require(['jquery', 'jquery/ui', 'slick'], function($) {
     $(document).ready(function() {
         $(".carousel > .pagebuilder-column-line").slick({
             infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
         });
     });
 });
