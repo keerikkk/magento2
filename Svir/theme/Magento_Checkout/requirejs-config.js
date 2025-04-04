ffdc2942d5b3c16fc29774c51693f1acf48f3a15
Kyrylo Svir <ksvir17@gmail.com>
2025-04-04 17:53:06 +0300

    STD-693
    - Add mixin to Update Shipping Methods when Street Field Input Change

--- Svir/theme/Magento_Checkout/requirejs-config.js
+++ Svir/theme/Magento_Checkout/requirejs-config.js
@@ -6,6 +6,9 @@ var config = {
             },
             'Magento_Checkout/js/view/summary/abstract-total': {
                 'Magento_Checkout/js/view/summary/abstract-total-mixin': true
+            },
+            'Magento_Checkout/js/model/shipping-rates-validation-rules': {
+                'Magento_Checkout/js/checkout/model/shipping-rates-validation-rules-mixin': true
             }
         }
     }
